38265ae6a50bc8d1de982c8dd58a62191a02b4a8
fernandovzfv <fernando.vazquez@telmexmail.com>
2024-09-03 16:08:05 -0600

    setup of deploy

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/me`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Fernando Vázquez',
   subHeading: 'Static | Heat Transfer | Package',
   // social
