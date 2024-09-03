b679bfe1c23abae7febd8b45f87994e7ce6f58b3
fernandovzfv <fernando.vazquez@telmexmail.com>
2024-09-03 16:56:55 -0600

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-1e8332f1fcd4121ce496.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_spectral=self.webpackChunkgatsby_starter_spectral||[]).push([[681],{7541:function(t,e,r){var a=r(4994);e.__esModule=!0,e.default=void 0;var n=a(r(6221)),u=a(r(6540)),l=function(t){function e(){return t.apply(this,arguments)||this}return(0,n.default)(e,t),e.prototype.render=function(){return u.default.createElement(u.default.Fragment,null)},e}(u.default.Component),s=l;e.default=s}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-1e8332f1fcd4121ce496.js.map
